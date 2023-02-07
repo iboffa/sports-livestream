207d17313ad0e5752f97b057cf18517150bf1a00
iboffa <i.boffa@hotmail.it>
2023-02-07 10:51:54 +0200

    app component demo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,12 +15,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-template'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-template');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
